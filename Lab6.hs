65b54d80cd9a91e036cec539ef7b8d236ca7d095
Yuri Kovach <108182448+YuriBazz@users.noreply.github.com>
2022-12-16 14:46:36 +0500

    Update Lab6.hs

--- Lab6.hs
+++ Lab6.hs
@@ -1,16 +1,16 @@
+
 module Lab6 where
--- ЕБАЛ Я В РОТ ЭТУ ЛАБУ
  import Data.List
  import Data.Maybe
 
  -- 1)
  class Monoid' a where
-    (<>) :: a -> a -> a
+    (<>.) :: a -> a -> a
     neutral :: a
     
-    {-# MINIMAL (<>), neutral #-}
+
  instance Monoid' [t] where
-    (<>) = (++)
+    (<>.) = (++)
     neutral = []
     
 
@@ -22,22 +22,29 @@ module Lab6 where
    deriving Show
  instance Monoid' Permutation where 
    neutral = NeutralPermutaion
-   x <> NeutralPermutaion =  x
-   NeutralPermutaion <> x = x
-   Permutation (x, l1) <> Permutation (y, l2) = 
+   x <>. NeutralPermutaion =  x
+   NeutralPermutaion <>. x = x
+   Permutation (x, l1) <>. Permutation (y, l2) = 
       if x == y 
-         then Permutation (x, f l1 l2)
-         else undefined
-         where f a b = map (\ (x, y) -> (x, fromJust $ lookup y b)) a 
+         then 
+          let 
+            list = f l1 l2
+            res = Permutation (x, list)
+          in
+            if and $ map (\(a,b) -> a == b) list
+              then NeutralPermutaion
+              else res
+         else error "Разная длина"
+      where f a b = map (\ (x, y) -> (x, fromJust $ lookup y b)) a 
 
 -- 3)  <> <- операция в моноиде 
 
  class Monoid' a => Group a where 
   opposite :: a -> a
-  {-# MINIMAL opposite #-}
+  
 
  instance Monoid' Double where
-   (<>) = (+)
+   (<>.) = (+)
    neutral = 0.0
  instance Group Double where 
    opposite = ((-1) *)
@@ -49,16 +56,23 @@ module Lab6 where
    deriving Show
  class Group a => LinerSpace a where 
    scal :: Double -> a -> a
-   {-# MINIMAL scal #-}
+   
  
  instance Monoid' Vec where
    neutral = NeutralVec
-   NeutralVec <> x = x
-   x <> NeutralVec = x
-   Vec (x, l1) <> Vec (y, l2) = 
+   NeutralVec <>. x = x
+   x <>. NeutralVec = x
+   Vec (x, l1) <>. Vec (y, l2) = 
       if x == y 
-         then Vec (x, zipWith (+) l1 l2)
-         else undefined
+         then 
+          let 
+            list = zipWith (+) l1 l2
+            res = Vec (x, list)
+          in
+            if and $ map (== 0) list
+              then NeutralVec
+              else res
+         else error "Разная длина"
 
  instance Group Vec where        
    opposite (Vec (x , l)) = Vec (x , map (* (-1)) l)
@@ -69,34 +83,31 @@ module Lab6 where
    scal _ NeutralVec = NeutralVec
    scal a (Vec (x, l)) = Vec (x, map (* a) l)
 
- 
- 
- 
- 
  instance Monoid' b => Monoid' (a -> b) where
    neutral = \_-> neutral
-   (<>) f g = \x -> f x <> g x
+   (<>.) f g = \x -> f x <>. g x
 
  instance Group b => Group (a -> b) where
    opposite f = \ x -> opposite $ f x
  
  instance LinerSpace b => LinerSpace (a -> b) where
   scal t f = \x -> scal t (f x)
-
--- 5) 
+-- 5)
 
  class LinerSpace a => HilbertSpace a where 
    (%) :: a -> a -> Double
+
  instance HilbertSpace Vec where
    Vec (x, l1) % Vec (y, l2) = 
       if x == y 
          then sum $ zipWith (*) l1 l2
-         else undefined
-   _ % NeutralVec = undefined
-   NeutralVec % _ = undefined
-   
+         else error "Разная длина"
+   _ % NeutralVec = 0
+   NeutralVec % _ = 0
+ 
  instance LinerSpace Double where 
-  scal a x = a * x
+  scal a f = a * f 
+
  instance HilbertSpace (Double -> Double) where
   f % g = 
     let 
